2e882918ddd2f3ddcddc66a4aaee53f87d6a69df
yikangGu <guyk@dgut.edu.cn>
2019-04-20 13:02:53 +0800

    update

--- 19-03-14/1.cpp
+++ 19-03-14/1.cpp
@@ -3,18 +3,67 @@
 
 using namespace std;
 
-int maxArea(vector<int> &height)
+bool cmp(char x, char y)
 {
-    int s = 0;
-    for (size_t i = 0; i < height.size(); i++)
-        for (size_t j = i + 1; j < height.size(); j++)
-            s = max((int)(min(height[i] , height[j])*(j-i)), s);
-    return s;
+    return x == y || y == '.';
+}
+
+bool isMatch(string s, string p)
+{
+    int idx = 0;
+    for (size_t i = 0; i < p.size(); i++)
+    {
+        if (s[0] == p[i] || s[0] == '.')
+        {
+            idx = i;
+            break;
+        }
+    }
+
+    int s_idx = 1;
+    bool flag = false;
+    for (size_t i = idx + 1; i < p.size(); i++)
+    {
+        if (p[i] == '*')
+        {
+            if (cmp(s[s_idx], p[i - 1]))
+            {
+                while (s_idx < s.size() - 1)
+                {
+                    flag = true;
+                    if (s[s_idx] == s[s_idx + 1] || p[i - 1] == '.')
+                        s_idx++;
+                    else
+                    {
+                        i++;
+                        s_idx++;
+                        break;
+                    }
+                }
+            }
+            else
+                return false;
+        }
+
+        if (cmp(s[s_idx], p[i]) && flag == true)
+            s_idx++;
+        else if (cmp(s[s_idx], p[i - 1]) && flag == false)
+            return true;
+        else
+            return false;
+    }
+
+    if (s_idx != s.size())
+        return false;
+    return true;
 }
 
 int main(int argc, char const *argv[])
 {
-    vector<int> data = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    cout << " max area: " << maxArea(data) << endl;
+    cout << " 1: " << isMatch("aa", "a") << endl;
+    cout << " 2: " << isMatch("aa", "a*") << endl;
+    cout << " 3: " << isMatch("ab", ".*") << endl;
+    cout << " 4: " << isMatch("aaaaaab", "c*a*b") << endl;
+    cout << " 5: " << isMatch("mississippi", "mis*is*p*.") << endl;
     return 0;
 }
